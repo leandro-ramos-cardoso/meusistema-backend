3f17c1d1e1bb443cb9554bbf3ff16abe6ab21599
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-09-02 12:56:47 -0300

    feat-list02

--- src/main/java/br/com/meusistema/api/service/FornecedorService.java
+++ src/main/java/br/com/meusistema/api/service/FornecedorService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +20,9 @@ public class FornecedorService {
 
     public List<Fornecedor> listarTodosFornecedores() {
         return fornecedorRepository.findAll();
+    }
+
+    public Optional<Fornecedor> buscarFornecedorPorId(Long id) {
+        return fornecedorRepository.findById(id);
     }
 }
